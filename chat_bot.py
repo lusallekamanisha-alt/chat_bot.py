d0568d42d1106fe65234b2c2c296e41c3d8071e6
lusallekamanisha-alt <lusallekamanisha@gmail.com>
2025-09-19 12:36:35 +0100

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -1,1 +1,13 @@
-Name: salle kamanisha Programme: BSC ICT Software Engineering Course: Fundamentals in Programming SIN: 241260606
+
+name = input("What is your name? ")
+
+print("Hello", name + "! Nice to meet you.)
+feeling = input("How are you feeling today? ")
+
+if feeling.lower() == "good":
+    print("That's great to hear! Keep smiling, " + name + "!")
+elif feeling.lower() == "bad":
+    print("I'm sorry to hear that, " + name + ". I hope things get better soon.")
+else:
+    print("Thank you for sharing, " + name + ".")
+
